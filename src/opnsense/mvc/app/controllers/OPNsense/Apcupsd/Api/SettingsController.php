8e550cb1adef835d2a8b7dfec43dbb7053fbd5b4
Gibbon99 <dberry999@gmail.com>
2021-03-14 14:34:27 +1100

    Change config get and set to preferred ApiMutableModelControllerBase

--- src/opnsense/mvc/app/controllers/OPNsense/Apcupsd/Api/SettingsController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Apcupsd/Api/SettingsController.php
@@ -2,56 +2,10 @@
 
 namespace OPNsense\apcupsd\Api;
 
-use OPNsense\Base\ApiControllerBase;
-use OPNsense\Apcupsd\apcupsd;
-use OPNsense\Core\Config;
+use \OPNsense\Base\ApiMutableModelControllerBase;
 
-class SettingsController extends ApiControllerBase
+class SettingsController extends ApiMutableModelControllerBase
 {
-    /**
-     * retrieve apcupsd general settings
-     * @return array general settings
-     */
-    public function getAction()
-    {
-        // define list of configurable settings
-        $result = [];
-        if ($this->request->isGet()) {
-            $mdlapcupsd = new apcupsd();
-            $result['apcupsd'] = $mdlapcupsd->getNodes();
-        }
-        return $result;
-    }
-
-    /**
-     * update apcupsd settings
-     * @return array status
-     */
-    public function setAction()
-    {
-        $result = ["message" => "failed to set"];
-        if ($this->request->isPost()) {
-            // load model and update with provided data
-            $mdlapcupsd = new apcupsd();
-            $mdlapcupsd->setNodes($this->request->getPost("apcupsd"));
-
-            // perform validation
-            $valMsgs = $mdlapcupsd->performValidation();
-            foreach ($valMsgs as $field => $msg) {
-                if (!array_key_exists("validations", $result)) {
-                    $result["validations"] = [];
-                }
-                $result["validations"]["general." . $msg->getField()] = $msg->getMessage();
-            }
-
-            // serialize model to config and save
-            if ($valMsgs->count() == 0) {
-                $mdlapcupsd->serializeToConfig();
-                Config::getInstance()->save();
-                $result["message"] = "Settings saved. Service will need to be restarted.";
-            }
-        }
-        return $result;
-    }
+	protected static $internalModelClass = '\OPNsense\Apcupsd\apcupsd';
+	protected static $internalModelName = 'apcupsd';
 }
-
