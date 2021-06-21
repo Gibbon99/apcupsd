0c6dfcf706ac0b823a34b08bfac341f4dd1a790b
Gibbon99 <dberry999@gmail.com>
2021-06-21 19:14:29 +1000

    Add missed script files

---
+++ src/opnsense/scripts/OPNsense/Apcupsd/getServiceStatus.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+
+#
+# Support script for the apcupsd plugin
+#
+# Get the current status of the APCUPSD service
+#
+
+import subprocess
+
+print (subprocess.getoutput("/usr/sbin/service apcupsd status"))
